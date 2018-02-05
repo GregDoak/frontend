fb55d88f34bc4b55117c2fbef224ea3b2c8f03d2
Greg Doak <github@gregdoak.com>
2018-02-05 09:29:23 +0000

    Added Fort Awesome and Core UI

--- src/app/core/config/fort-awesome.config.ts
+++ src/app/core/config/fort-awesome.config.ts
@@ -2,13 +2,7 @@ import fontawesome from '@fortawesome/fontawesome';
 
 fontawesome.config.searchPseudoElements = true;
 
-// Icons should be imported individually to keep bundle size down
-// import faPhone from '@fortawesome/fontawesome-free-solid/faPhone';
-// fontawesome.library.add(faPhone);
+import solid from '@fortawesome/fontawesome-free-solid';
 
-// If really necessary, entire styles can be loaded instead of specifying individual icons
-// import solid from '@fortawesome/fontawesome-free-solid';
-// fontawesome.library.add(solid);
+fontawesome.library.add(solid);
 
-// Execute SVG replacement
-// fontawesome.dom.i2svg();
