58454e22cef716ba2c8c66eee6ad1a35fb930ff4
Greg Doak <me@gregdoak.com>
2018-06-08 15:50:23 +0100

    Fixing small issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
+var apiHost = 'localhost:8000';
+var apiURL = 'http://' + apiHost + '/api/';
 switch (process.env.NODE_ENV) {
   case 'live':
   case 'prod':
   case 'production':
-    apiHost = 'localhost:8000';
-    apiURL = 'http://' + apiHost + '/api/';
     module.exports = require('./config/webpack.prod')({
       ENV: 'prod',
       API_HOST: apiHost,
@@ -12,8 +12,6 @@ switch (process.env.NODE_ENV) {
     break;
   case 'test':
   case 'testing':
-    apiHost = 'localhost:8000';
-    apiURL = 'http://' + apiHost + '/api/';
     module.exports = require('./config/webpack.test')({
       ENV: 'test',
       API_HOST: apiHost,
@@ -23,8 +21,6 @@ switch (process.env.NODE_ENV) {
   case 'dev':
   case 'development':
   default:
-    apiHost = 'localhost:8000';
-    apiURL = 'http://' + apiHost + '/api/';
     module.exports = require('./config/webpack.dev')({
       ENV: 'dev',
       API_HOST: apiHost,
